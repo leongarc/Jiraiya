b934180d74578fbd5ef59f49f99840740c078c7b
Leo <leo@Leos-MacBook-Pro-2.local>
2024-07-11 00:45:07 -0700

    Added Team model

--- jiraiya/api/models.py
+++ jiraiya/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=15, unique=True)
+    player_count = models.IntegerField(default=0)
+    coach_name = models.CharField(max_length=25, default="None")
+    goals_scored = models.IntegerField(max_length=1000, default=0)
+    
