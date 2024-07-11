bb5e783295e35fb5cec845ca0f997151b6cf0ba0
Leo <leo@Leos-MacBook-Pro-2.local>
2024-07-11 00:49:11 -0700

    Migrated changes in api model

--- jiraiya/api/models.py
+++ jiraiya/api/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class Team(models.Model):
     team_name = models.CharField(max_length=15, unique=True)
     player_count = models.IntegerField(default=0)
-    coach_name = models.CharField(max_length=25, default="None")
-    goals_scored = models.IntegerField(max_length=1000, default=0)
-    
+    coach_name = models.CharField(max_length=25, default="none")
+    goals_scored = models.IntegerField(default=0)
